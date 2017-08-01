40c51c2a4408d82e93ffe9b8d5a4cf2324cecc37
tzmanics <tzmanics@gmail.com>
2017-08-01 11:22:28 -0400

    initial commit with form added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdM04Dbu4jyIGOs4mmvNLIwbCKuzq5v1EkPDbDuBvcfEIhd8g/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0" title="cwwc-signup-form">Loading...</iframe>
+      </div>
+    );
+  }
+}
+
+export default App;
